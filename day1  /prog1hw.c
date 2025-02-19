665b7f0d8fd33702b0757f918486e55b95e95cd0
Vinush kumar <149343439+Vinush911@users.noreply.github.com>
2025-02-19 14:20:32 +0530

    Create  prog1hw.c

---
+++ day1  /prog1hw.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int status;
+    
+    // Prompt the user to enter the order status
+    printf("Enter order status (1-4): ");
+    scanf("%d", &status);
+    
+    // Display the corresponding message
+    switch (status) {
+        case 1:
+            printf("Your order has been placed.\n");
+            break;
+        case 2:
+            printf("Your food is being prepared.\n");
+            break;
+        case 3:
+            printf("Your order is out for delivery.\n");
+            break;
+        case 4:
+            printf("Your order has been delivered. Enjoy your meal!\n");
+            break;
+        default:
+            printf("Invalid status. Please enter a number between 1 and 4.\n");
+    }
+    
+    return 0;
+}
