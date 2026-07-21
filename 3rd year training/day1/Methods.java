27fbfed84b4de181b61d1dc2be62c0587e784c12
Vinush kumar <149343439+Vinush911@users.noreply.github.com>
2026-07-21 14:24:54 +0530

    Add files via upload

---
+++ 3rd year training/day1/Methods.java
@@ -0,0 +1,12 @@
+public class Methods {
+    public static void main(String[] args) {
+        
+        System.out.println(add(2,4));
+        System.out.println(add(2,4));
+
+    }
+    public static int add(int num1,int num2) {
+        int sum=num1+num2;
+        return sum;
+    }
+}
---
+++ 3rd year training/day1/loops.java
@@ -0,0 +1,15 @@
+public class loops {
+    public static void main(String[] args) {
+        //duplicate
+        // System.out.println(1);
+        // System.out.println(2);
+        // System.out.println(3);
+        // System.out.println(4);
+        // System.out.println(5);
+
+        //loop
+        for (int num =0;num<10;num++){
+            System.out.println(num);
+        }
+    }
+}
